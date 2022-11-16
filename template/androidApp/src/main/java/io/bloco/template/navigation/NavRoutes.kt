5c6d9d869f9455afae7bf0e3ec3327c9ff33c1d0
bloco <filipe@bloco.io>
2022-11-16 18:37:16 +0000

    updated: README.md && fixed: lint

--- template/androidApp/src/main/java/io/bloco/template/navigation/NavRoutes.kt
+++ template/androidApp/src/main/java/io/bloco/template/navigation/NavRoutes.kt
@@ -5,11 +5,10 @@ sealed class NavRoutes(internal open val path: String) {
     object List : NavRoutes("/")
     object Details : NavRoutes("details/{$DETAILS_ID_KEY}") {
         fun build(id: String): String =
-            path.replace("{${DETAILS_ID_KEY}}", id)
+            path.replace("{$DETAILS_ID_KEY}", id)
     }
 
     companion object {
         const val DETAILS_ID_KEY: String = "id"
     }
 }
-
